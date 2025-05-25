2d3a9c3bbd6aba5bb153ab3cce0c5ddcab7c5d74
Bartlomiej Kozka <bartek.kozka15@gmail.com>
2025-05-25 18:52:21 +0200

    minor amend

--- src/GearShift.cpp
+++ src/GearShift.cpp
@@ -3,6 +3,8 @@
 #include "FuzzyInputs.hpp"
 
 #include <iostream>
+#include <algorithm>
+
 
 // 1. calculate the probabilities for velocity, engine RPM, and throttle level
 // 2. choose only probabilities over 0
@@ -16,17 +18,6 @@ float const GearShift::shift()
     std::unordered_map<Velocity::level, float> velocityProbabilities = calcProbabilities<Velocity>(velocity);
     std::unordered_map<EngineRpm::level, float> engineRpmProbabilities = calcProbabilities<EngineRpm>(engineRpm);
     std::unordered_map<ThrottleLevel::level, float> throttleLevelProbabilities = calcProbabilities<ThrottleLevel>(throttleLevel);
-    std::cout << "Velocity probabilities: " << velocityProbabilities.size() << std::endl;
-    for (const auto& [v, prob] : velocityProbabilities) 
-    {
-        std::cout << "Velocity: " << static_cast<int>(v) << ", Probability: " << prob << std::endl;
-    }
-    std::cout << "Engine RPM probabilities: " << engineRpmProbabilities.size() << std::endl;
-    for (const auto& [r, prob] : engineRpmProbabilities) 
-    {
-        std::cout << "Engine RPM: " << static_cast<int>(r) << ", Probability: " << prob << std::endl;
-    }
-    std::cout << "Throttle Level probabilities: " << throttleLevelProbabilities.size() << std::endl;
 
     std::unordered_map<Gear::level, float> gearProbabilities;
     for (const auto& [v, vProb] : velocityProbabilities) 
@@ -38,7 +29,6 @@ float const GearShift::shift()
                 auto rule = std::make_tuple(v, r, t);
                 if (FuzzyRules::rules.find(rule) != FuzzyRules::rules.end()) 
                 {
-                    std::cout << "FOUND" << std::endl;
                     Gear::level gear = FuzzyRules::rules.at(rule);
                     float probability = std::min({vProb, rProb, tProb});
                     if (gearProbabilities.find(gear) != gearProbabilities.end()) 
@@ -61,10 +51,5 @@ float const GearShift::shift()
         }
     }
 
-    std::cout << gearProbabilities.size() << std::endl;
-    for (const auto& [gear, prob] : gearProbabilities) 
-    {
-        std::cout << "Gear: " << static_cast<int>(gear) << ", Probability: " << prob << std::endl;
-    }
     return defuzzifyWeightedAverage(gearProbabilities, Gear::ranges);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include "GearShift.hpp"
 #include <iostream>
+#include "CLI/CLI.hpp"
 
 
-int main()
+int main(int argc, char **argv)
 {
+    CLI::App app{"App description"};
+
+    int v = 0;
+    int r = 0;
+    int t = 0;
+    app.add_option("-v", v, "Velocity (0-200)");
+    app.add_option("-r", r, "Rpm (0-8000)");
+    app.add_option("-t", t, "Throttle (0-100)");
+
+    CLI11_PARSE(app, argc, argv);
+
     GearShift gearShift;
-    gearShift.setInputs(90, 1000, 100); 
+    gearShift.setInputs(v, r, t); 
     float newGear = gearShift.shift();
-    std::cout << "Recommended Gear: " << newGear << std::endl;
 
     return 0;
 }
